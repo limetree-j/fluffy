be9e451450f02da52de44c0a4b7a09d210c57786
limetree-j <limetree.jdh@gmail.com>
2022-01-09 17:08:34 +0900

    Initial Commit

---
+++ fluffy/src/main/java/com/fluffytrio/fluffy/FluffyApplication.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.fluffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyApplication.class, args);
+	}
+
+}
---
+++ fluffy/src/test/java/com/fluffytrio/fluffy/FluffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.fluffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
